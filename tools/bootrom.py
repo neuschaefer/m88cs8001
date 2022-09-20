f5f40bf0d47a92873753bd1525561cfc4a5afe03
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2022-09-21 00:17:20 +0200

    tools/bootrom.py: Add --listen-once option and refactor
    make && tools/bootrom.py serprog/serprog.bin --listen-once && flashrom -p serprog:dev=/dev/ttyUSB0

--- tools/bootrom.py
+++ tools/bootrom.py
@@ -12,17 +12,17 @@ def hexdump(data):
             line += ''.join([chr(x) if (x >= 0x20 and x <= 0x7f) else '.' for x in d])
             print(line)
 
-def load_boot1(filename, devname, listen, do_hexdump):
-    s = serial.Serial(devname, baudrate=115200, timeout=0.1)
+def load_boot1(args):
+    s = serial.Serial(args.dev, baudrate=115200, timeout=0.1)
 
-    program = bytearray(open(filename, 'rb').read())
+    program = bytearray(open(args.filename, 'rb').read())
     if len(program) <= 0x400:
         printf(f'Program too short ({len(program):#x} bytes), this is not going to work!')
         return
 
     program[0x00:0x04] = struct.pack('<I', 0x5a7d9cbf)
     program[0x14:0x18] = struct.pack('<I', len(program) - 0x300)
-    if do_hexdump:
+    if args.hexdump:
         hexdump(program)
 
     while True:
@@ -41,13 +41,15 @@ def load_boot1(filename, devname, listen, do_hexdump):
             print('\nTEXT:')
             print(text.decode('ascii'))
             break
-    if listen:
+    if args.listen or args.listen_once:
         while True:
+            time.sleep(0.1)
             text = s.read_all()
             if text != b'':
                 sys.stdout.buffer.write(text)
                 sys.stdout.buffer.flush()
-            time.sleep(0.1)
+            if args.listen_once:
+                break
 
 
 if __name__ == '__main__':
@@ -56,11 +58,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Load bootloader stage 1 (boot1) via UART')
     parser.add_argument('filename', help='filename of the boot1 image')
     parser.add_argument('--hexdump', help='hexdump the program', action='store_true')
-    parser.add_argument('--dev', help='device path (default: /dev/ttyUSB0)')
-    parser.add_argument('--listen', help='listen for output ', action='store_true')
+    parser.add_argument('--dev', help='device path (default: /dev/ttyUSB0)', default='/dev/ttyUSB0')
+    parser.add_argument('--listen', help='listen for output', action='store_true')
+    parser.add_argument('--listen-once', help='listen for output shortly, then exit', action='store_true')
     args = parser.parse_args()
 
-    if args.dev:
-        devname = args.dev
-
-    load_boot1(args.filename, devname, args.listen, args.hexdump)
+    load_boot1(args)
